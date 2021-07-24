cfba4e023015a9660a999791760d78419e28da43
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-24 01:35:44 -0300

    feat: implemented a function that uses streams

--- service/user.go
+++ service/user.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/LuizEduardoCardozo/fullcycle-go-grpc-example/pb"
 )
@@ -24,5 +25,35 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+
+	stream.Send(&pb.UserResultStream{
+		Result: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 1)
+
+	stream.Send(&pb.UserResultStream{
+		Result: "Found a name",
+		User: &pb.User{
+			Name: "Eduardo",
+		},
+	})
+
+	time.Sleep(time.Second * 1)
+
+	stream.Send(&pb.UserResultStream{
+		Result: "Found a email",
+		User: &pb.User{
+			Name:  "Eduardo",
+			Email: "eduard.cardoz@gmail.com",
+		},
+	})
+
+	return nil
 
 }
